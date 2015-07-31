10062df66c101ceef29572758fec9fc1d9c43c22
nicholasruggeri <ruggerinicholas@gmail.com>
2015-07-31 17:47:28 +0200

    refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,137 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer'),
-    cp = require('child_process'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    runSequence = require('run-sequence');
 
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var exec = require('child_process').exec;
+var uglify = require('gulp-uglify');
+var gulpif = require('gulp-if');
+var concat = require("gulp-concat");
+var sourcemaps = require('gulp-sourcemaps');
+var runSequence = require('run-sequence');
+
+
+/**
+ * Project Configuration
+ * =====================
+ */
+
+var prod = false,
+    basePath = 'src',
+    vendorPath = basePath + '/_assets/_vendor',
+    jsplugins = [
+        vendorPath + '/jquery/dist/jquery.js',
+    ];
 
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
+gulp.task('jekyll-build', function () {
+    exec('jekyll build', function(err, stdout, stderr) {
+        browserSync.notify('jekyll build');
+        browserSync.reload();
+    });
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', function () {
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
     browserSync.reload();
-    runSequence('jekyll-build', ['js', 'js:vendor', 'sass', 'img']);
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+    console.log('enter bs')
     browserSync({
         server: {
-            baseDir: 'site'
+            baseDir: 'web'
         }
     });
 });
 
+
+
+
+
 /**
- * Compile files from _assets/_scss into site/css (for live injecting)
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_assets/_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('site/css'))
-        .pipe(browserSync.reload({stream:true}));
+        return gulp.src(basePath + '/_assets/_scss/style.scss')
+            .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(sass())
+        .pipe(prefix(['last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'], { cascade: true }))
+        .pipe(gulp.dest('web/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest(basePath + '/css'))
 });
 
 /**
-* Compile files from _assets/_js into site/js (for live injecting)
- */
-gulp.task('js', function(){
-    return gulp.src('_assets/_js/**/*.js')
-        .pipe(concat('main.min.js'))
+* Concat compress js
+*/
+gulp.task('js', function() {
+    return gulp.src(basePath + '/_assets/_js/**/*.js')
+        .pipe(gulpif(prod, uglify({
+            preserveComments: 'some'
+        })))
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest(basePath + '/js'));
+});
+
+
+
+
+
+
+/**
+* Concat compress js
+*/
+gulp.task('js:vendor', function() {
+    return gulp.src(jsplugins)
         .pipe(uglify())
-        .pipe(gulp.dest('site/js'))
-        .pipe(browserSync.reload({stream:true}));
-});
-
-/**
-* Compile files from _assets/_vendor into site/js, site/css ecc (for live injecting)
- */
-gulp.task('js:vendor', function(){
-    return gulp.src([
-            '_assets/_vendor/cookies-enabler/cookies-enabler.min.js'
-        ])
-        .pipe(concat('vendor.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('site/js'))
-        .pipe(browserSync.reload({stream:true}));
-});
-
-/**
- * Copy files from _assets/_img into site/img (for live injecting)
- */
-gulp.task('img', function() {
-    return gulp.src(['_assets/_img/**/*'])
-        .pipe(gulp.dest('site/img'))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(basePath + '/js'));
 });
 
 
+
+
+
+
+
+
+
+
 /**
- * Watch files, run jekyll & reload BrowserSync
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_assets/_scss/*.scss', ['sass']);
-    gulp.watch('_assets/_js/**/*.js', ['js']);
-    gulp.watch('_assets/_img/**/*', ['img']);
-    gulp.watch('_src/**/*.html', ['jekyll-rebuild']);
+    gulp.watch(basePath + '/_assets/_js/**/*.js', ['js', 'jekyll-build']);
+    gulp.watch(basePath + '/_assets/_scss/**/*.scss', ['sass']);
+    gulp.watch(basePath + '/**/*.html', ['jekyll-rebuild']);
+    gulp.watch(basePath + '/img/**/*', ['jekyll-rebuild']);
 });
 
-gulp.task('build', function(callback) {
-    runSequence('jekyll-build', ['js', 'js:vendor', 'sass', 'img']);
-});
-
-
-
 /**
- * Default task, running just `gulp` will compile the assets and compile the jekyll site
- */
-
-gulp.task('default', ['build']);
-
-/**
- * Running just `gulp serve` will compile the sass,
+ * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
+gulp.task('default', ['browser-sync', 'watch']);
 
-gulp.task('serve', function(callback) {
-    runSequence('jekyll-build', ['js', 'js:vendor', 'sass', 'img', 'browser-sync'], 'watch');
+
+/**
+* Gulp Prod
+* =========
+* Prod
+*/
+
+gulp.task('prod', function() {
+    prod = true;
+    runSequence('sass', 'js', 'js:vendor', 'jekyll-build')
 });
